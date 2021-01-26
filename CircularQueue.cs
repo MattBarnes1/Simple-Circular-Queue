5a1cdf494b869e51fe677d02edfed1dc1167ecbe
MattBarnes1 <mpb38zipsuakronedu@gmail.com>
2021-01-25 20:54:31 -0500

    First Commit

---
+++ CircularQueue.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CircularQueue<T> : IEnumerable<T>
+{
+    int LastSetItemPointer = 0;
+    T[] myQueue;
+    uint lastUsed = 0;
+    public CircularQueue(uint QueueSize)
+    {
+        myQueue = new T[QueueSize];
+    }
+
+    HashSet<T> myObjects = new HashSet<T>();
+    public void Resize(uint Capacity)
+    {
+        Array.Resize(ref myQueue, (int)Capacity);
+    }
+    int ItemCount = 0;
+    public uint Count { get { return (uint)ItemCount; } }
+
+    public void SwapPosition(ref T ReturnedItem, int Position)
+    {
+        var ReturnVal = myQueue[Position];
+        myQueue[Position] = ReturnedItem;
+        ReturnedItem = ReturnVal;
+        return;
+    }
+
+    public int Insert(T myItem, ref T ReturnVal)
+    {
+        int Position = -1;
+        myObjects.Add(myItem);
+        ReturnVal = myQueue[LastSetItemPointer];
+        Position = LastSetItemPointer;
+        if (ReturnVal == null)
+            ItemCount++; //it's a new Item
+        else
+            myObjects.Remove(ReturnVal);
+        myQueue[LastSetItemPointer] = myItem;
+        LastSetItemPointer++;
+        if (LastSetItemPointer == myQueue.Length)
+        {
+            LastSetItemPointer = 0;
+        }
+        return Position;
+    }
+
+
+
+    public int Insert(T myItem)
+    {
+        int Position = -1;
+        myObjects.Add(myItem);
+        var ReturnVal = myQueue[LastSetItemPointer];
+        Position = LastSetItemPointer;
+        if (ReturnVal == null)
+            ItemCount++; //it's a new Item
+        else
+            myObjects.Remove(ReturnVal);
+        myQueue[LastSetItemPointer] = myItem;
+        LastSetItemPointer++;
+        if (LastSetItemPointer == myQueue.Length)
+        {
+            LastSetItemPointer = 0;
+        }
+        return Position;
+    }
+
+
+    public T this[uint position]
+    {
+        get
+        {
+            return myQueue[position];
+        }
+        set
+        {
+            myQueue[position] = value;
+        }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        return myQueue.GetEnumerator();
+    }
+
+    IEnumerator<T> IEnumerable<T>.GetEnumerator()
+    {
+        return myQueue.GetEnumerator() as IEnumerator<T>;
+    }
+
+
+}
